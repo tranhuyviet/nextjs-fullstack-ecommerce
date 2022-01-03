259888624eb4f82bb9c04b89bc908cf2cb1d54d9
tranhuyviet <vietth@msn.com>
2022-01-03 12:09:58 +0200

    _

--- services/productService.ts
+++ services/productService.ts
@@ -42,10 +42,13 @@ const getAllProducts = async (
         query = { $and: arrayAND };
     }
 
-    return Product.find(query).skip(skip).limit(limit).populate(
-        //     { path: 'variants', select: 'name colorHex' },
-        { path: 'sizes', select: 'name' }
-    );
+    return Product.find(query)
+        .skip(skip)
+        .limit(limit)
+        .populate([
+            { path: 'variants', select: 'name colorHex', model: 'variants' },
+            { path: 'sizes', select: 'name', model: 'sizes' },
+        ]);
 };
 
 // calculate total number of products
